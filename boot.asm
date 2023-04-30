7fe9cd6dfec099747d9889eb75feff456fe75e1b
Boris <ceranic.boris2@gmail.com>
2023-04-30 19:23:42 +0200

    Added more to boot.asm Added a memory map reference when the computer is in boot for the future

--- boot.asm
+++ boot.asm
@@ -3,12 +3,12 @@
 global start
 
 
-
-;;variables for the fat16 file system
+;;========================Variables for the FAT16 file system==========================
 %define jump_point 0x3C
 %define OEM_ID 'BSD  4.4' 
 %define bytes_per_sector 512
-%define sectors_per_clutser 1
+;in code we assume this is always 1
+%define sectors_per_clutser 1 
 %define reserved_sectors 1
 %define num_of_FATs 1
 ;each directory is 32
@@ -36,10 +36,11 @@ global start
 %define volume_start reserved_sectors
 %define FAT_start volume_start + reserved_sectors 
 %define root_dir FAT_start+num_of_FATs*sectors_per_FAT
-%define root_dir_size (num_of_root_dir*32)/512;
+%define root_dir_size (num_of_root_dir*32)/512
 ;;--------------------------------------------------------
 ;;Define where to put the sectors
 %define search_sector_address 0x7E00
+%define disk_packet_struct 0x8000
 ;;============================================================
 ;;now we define the bytes for the file system
 ;;jump point
@@ -75,8 +76,8 @@ db file_system_type
 ;
 start:
     xor ax,ax
-    xor ds, ds
-    xor ss,ss
+    mov ds, ax
+    mov ss,ax
     mov sp, 0x9c00 ; this is arbitrary and the stack could be whatever it wants??
     
     cli 
@@ -99,39 +100,57 @@ pmode:
 unreal:
     pop ds
     sti
-    mov bx,
+    mov bx, 0
 
     ;;here the code starts, we are in unral mode and everything is okay
     ;;we need to find loader.sys which should be in the root dir
+    ;------------------------we check if we have int 13h extentions-------------------------
+        mov ah, 0x41
+        mov bx,0x55AA
+        mov dl, 0x80 ;; TODO: ged the actual reading from DL, from the bios
+        int 0x13
+        jc no_int13_ext ;;else we are good and do not need to print anything
 
+
+
+    ;--------------------------------------------------------------------------------------
 
     ;-----------------------we set up the loop---------------------------
-    mov dx, root_dir
+    mov edx, root_dir*512
+    mul edx, bytes_per_sector
     mov bx, search_sector_address
-    add bx, 512
+    add bx, bytes_per_sector
 
     
     ;-------------------------------------------------------------------
     .loop:
-        cmp bx, (512+search_sector_address)
+        cmp edx, ((root_dir + root_dir_size) * 512)
+        jge 
+
+        cmp bx, (512+search_sector_address) ;;check if we need to load a new sector
         jl .no_new_sector 
         ;-----------------------------we have to load the new sector--------------------------------
 
-        ;;TODO Check if int 13h extensions are available
-        ;;and if they are search proparty man
-         mov ah, 0x02
-        mov al, 1
-        mov ch,0
-    
-        mov dh,0
-        mov cl,2
-        mov dl, 0x80
+        
+        mov [disk_packet_struct], 16 ;size of packet is 16 bytes
+        mov [disk_packet_struct+1],0 ; always 0
+        mov word [disk_packet_struct+2],1 ; number of sectors to transfer
+        mov word [disk_packet_struct+4], (search_sector_address) ;offset of placement
+        mov word [disk_packet_struct+6], 0 ;segment of placement
+        mov dword [disk_packet_struct+8] , edx
+        mov word [disk_packet_struct+12],0
+
+            ;----------------------We call int 13h-----------------------------
+            mov ah, 0x42
+            mov dl, 0x80 ;;TODO make this flexible
+            mov ds, (disk_packet_struct/16)
+            mov si, 0
+            int 0x13
+            ;---------------------------------------------------------
+
+        
+
         mov bx, search_sector_address
-        int 0x13
-
-
-
-
         ;----------------------------------------------------------------------------------------
         .no_new_sector:
         jmp is_loader
@@ -139,12 +158,38 @@ unreal:
 
         found_loader: ; congrats we have found the loader
             ;;load it into memory;;TODO
+            jmp load_loader
 
         no_loader_found:
-        ;;----------------------------we go to the next 
+        
+
         add bx,32
+    jmp .loop
+    .end: ; we assume no loader foudnd
+    mov ah, 0x0E
+	mov bh, 0x00
+
+    ;we shall unroll the code for space reasons
+    mov al, 'N'
+    int 10h
+    mov al, 'O'
+    int 10h
+    mov al, 'L'
+    int 10h
+    mov al, 'A'
+    int 10h
+    mov al, 'D'
+    int 10h
+    mov al, 'E'
+    int 10h
+    mov al, 'R'
+    int 10h
 
 
+jmp $
+
+load_loader:
+ 
 jmp $
 
 ;;we have it in memory at bx
@@ -184,6 +229,33 @@ is_loader:
 
     jmp found_loader
 
+
+;we go here if int 13 ext is not supported
+no_int13_ext:
+    mov ah, 0x0E
+	mov bh, 0x00
+
+    ;we shall unroll the code for space reasons
+    mov al, 'O'
+	int 10h
+    mov al, 'N'
+	int 10h
+	mov al, 'I'
+	int 10h
+    mov al, 'N'
+	int 10h
+    mov al, 'T'
+	int 10h
+    mov al, '1'
+	int 10h
+    mov al, '3'
+	int 10h
+    mov al, 'E'
+	int 10h
+    mov al, 'X'
+	int 10h
+    mov al, 'T'
+	int 10h
 
 gdtinfo:
    dw gdt_end - gdt - 1   ;last byte in table
---
+++ memory map at boot.md
@@ -0,0 +1,5 @@
+## Here we have the memory map at boot##
+
+0x7C00 : MBR
+0x7D00 : Temporary sector for file searching and loading
+0x8000 : Disk address packed structure for LBA reading int 13h
