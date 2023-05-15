6bef652c3e48bd0c62a206f1b450d88ebdd82aec
Boris <ceranic.boris2@gmail.com>
2023-05-15 18:37:33 +0200

    Debug

--- OS/loader.c
+++ OS/loader.c
@@ -384,7 +384,7 @@ void prints(char* ptr, size_t len){
 
 char hex[16] = "0123456789ABCDEF";
 void dmph(char* ptr, size_t len){
-    printf((int)(*ptr));
+    printf((*ptr));
     // for(int i =0;i<len;i++){
         
     //     printch(hex[(*ptr)/16]);
