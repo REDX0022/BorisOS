5fac7db98c83dd4756e65aadeb5fb6e13d7a36c5
Boris <ceranic.boris2@gmail.com>
2023-05-16 23:28:22 +0200

    Debug

--- OS/loader.c
+++ OS/loader.c
@@ -198,6 +198,7 @@ int load_sector_helper(struct disk_packet *ptr){
         "shr esi, 28 \n "
         "int 0x13 \n "
         "jnc skip \n"
+        "mov bh, 0 \n"
         "mov al, 'f' \n"
         "mov ah, 0x0e \n"
         "int 0x10 \n"
