5874484f84f2bd18bef6df6ced07fcc9b5fcbf3a
Boris <ceranic.boris2@gmail.com>
2023-05-16 23:49:40 +0200

    Debug

--- OS/loader.c
+++ OS/loader.c
@@ -65,17 +65,17 @@
 
  struct directory{ //TODO: put unsigned where needed
     char name[11]; //the ext is also here 
-    int8_t attribute;
-    int8_t NT_reserved;
-    int8_t creation_stamp;
-    int16_t creation_time;
-    int16_t createion_date;
-    int16_t last_access_date;
-    int16_t reserved_for_fat32;
-    int16_t last_write_time;
-    int16_t last_write_date;
-    int16_t starting_cluster;
-    int16_t file_size_in_bytes; //
+    uint8_t attribute;
+    uint8_t NT_reserved;
+    uint8_t creation_stamp;
+    uint16_t creation_time;
+    uint16_t creation_date;
+    uint16_t last_access_date;
+    uint16_t reserved_for_fat32;
+    uint16_t last_write_time;
+    uint16_t last_write_date;
+    uint16_t starting_cluster;
+    uint32_t file_size_in_bytes; //
 };
 
 
