2f881242c98771cd45a202645e2dcf75c2f527fe
Boris <ceranic.boris2@gmail.com>
2023-05-17 00:13:52 +0200

    Debug

--- OS/MEMMNG.C
+++ OS/MEMMNG.C
@@ -15,6 +15,7 @@ struct alloc_segment memory[max_memory_sectors];
 
 
 void __start__(){
+    printch('m');
     init_memory_manager();
 }
 
@@ -123,20 +124,45 @@ void memcpy(void *src,void *dest,size_t n){
 void printf(int n){
     if(n==0){printch('0');return;}
     printf(n/10);
-    printch((char) (n%10)+48); //48 is the offset of the char 0
+    char tmpprint = (char)((n%10)+48);
+    printch(tmpprint); //48 is the offset of the char 0
     n/=10;
     
-
-
 }
 
-void printch(char c){
-    asm("push eax \n"
-        "mov eax, [bp+8] ; +4 for eax +4 for ebp \n"
+void printch(char c){ //i dont know it its needed to push, its for safety
+    asm("pushad \n"
+        "mov bh, 0x00 \n"
+        "mov al, [bp+8] ; +4 for ebx,  \n"
         "mov ah, 0x0e \n"
         "int 0x10 \n"
+        "popad \n"
     );
 }
+
+
+void prints(char* ptr, size_t len){
+    for(int i =0;i<len;i++){
+        printch(*(ptr++));
+    }
+}
+
+char hex[16] = "0123456789ABCDEF";
+void dmph(char* ptr, size_t len){
+    for(int i =0;i<len;i++){
+        char high = (char)(hex[((*ptr)&0xFF)>>4]);
+        char low = (char)(hex[(*ptr)&0xF]);
+    
+        printch(high);
+        printch(low);
+        printch(' ');
+        ptr++;
+    }
+    
+}
+
+
+
 
 //we need to make a primitive version of a linked list without using linked list because we dont have malloc
 
