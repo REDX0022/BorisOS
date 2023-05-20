ccdcc427362d4dc6f67418dc6217f2ed7aaca410
Boris <ceranic.boris2@gmail.com>
2023-05-20 11:39:16 +0200

    Debug

--- compile.mk
+++ compile.mk
@@ -4,10 +4,10 @@ targets := LOADER.SYS MEMMNG.SYS transfer
 run: $(targets)
 
 MEMMNG.SYS: OS/MEMMNG.C $(kernel_libs)
-	smlrcc -unreal LIBS/c0du.asm OS/MEMMNG.C -o MEMMNG.SYS -map MEMMNG.PMA
+	smlrcc -unreal LIBS/c0du.asm OS/MEMMNG.C -o MEMMNG.SYS -map MEMMNG.PMA 
 
 LOADER.SYS: /OS/LOADER.C $(kernel_libs)
-	smlrcc -unreal LIBS/c0du.asm OS/LOADER.C -o LOADER.SYS
+	smlrcc -unreal LIBS/c0du.asm OS/LOADER.C -o LOADER.SYS -I LIBS/
 
 transfer:
 	git commit -am Debug 
