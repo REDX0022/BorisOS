541079a5c0431c3d0f9da8caf90021a2b47995a2
Boris <ceranic.boris2@gmail.com>
2023-05-21 00:23:38 +0200

    Debug

--- SRC/FILEMNG/linked_header_FILEMNG.c
+++ SRC/FILEMNG/linked_header_FILEMNG.c
@@ -5,7 +5,7 @@ asm("times 16 db 0"); //null terminated
 
 extern void start_program();
 
-extern void init_MEMMNG(void* funcs);
+extern void init_MEMMNG(void** funcs);
 
 void __start__(void*** libs){
     init_MEMMNG(libs[0]);
