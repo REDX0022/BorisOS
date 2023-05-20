0b048adddcd4e384311ed18e476dcdfb558874ea
Boris <ceranic.boris2@gmail.com>
2023-05-21 00:54:38 +0200

    Debug

--- SRC/loader.c
+++ SRC/loader.c
@@ -272,7 +272,7 @@ int start_kernel_programm(void *start){
     
     void*** lib_store = &temp_sector; //where in the temp sector to put the funcs
     for(char (*lib_search)[16] = mz->lib_name; (*lib_search)[0]; lib_search++){
-        prints(mz->lib_name,11);
+        prints((char*)mz + 32,11);
         struct shared_lib* sh = get_shared_lib(lib_search);
         if(sh==NULL){
             return 2; // LIB NOT FOUND
