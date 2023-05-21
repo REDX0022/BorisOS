76c8514e266de7ddf7bcbd834780fae6d2cee0d7
Boris <ceranic.boris2@gmail.com>
2023-05-21 20:49:09 +0200

    Debug

--- SRC/FILEMNG/FILEMNG.C
+++ SRC/FILEMNG/FILEMNG.C
@@ -437,6 +437,7 @@ size_t dir_size(struct directory folder){
     }
     //then the current cluster points to the next one
     load_sector(data_start+(cur_cluster-2),(void*) &temp_sector);
+    //
     for(int i =0;i<bytes_per_sector;i+=32){
         if(!temp_sector[i]){//its the end of the file
             return cluster_count*bytes_per_sector+i;
