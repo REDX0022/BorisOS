5ff553a38a4032bdd746bbadc00bc03a94e01f1e
Boris <ceranic.boris2@gmail.com>
2023-05-21 11:34:14 +0200

    Debug

--- SRC/FILEMNG/FILEMNG.C
+++ SRC/FILEMNG/FILEMNG.C
@@ -537,7 +537,7 @@ void start_program(){
     nl();
     //===========FILE MNG TESTING================
     struct directory* root_listed = list_root();
-    printf(33);
+    nl();
     dmph((char*)root_listed,root_dir_size*bytes_per_sector,16);
 
 
