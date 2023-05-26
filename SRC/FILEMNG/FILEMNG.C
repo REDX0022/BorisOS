eb3bc74e6a6e281317f0a82ce1517ea9d041760a
REDX0022 <ceranic.boris2@gmail.com>
2023-05-26 12:42:57 +0200

    Debug

--- SRC/FILEMNG/FILEMNG.C
+++ SRC/FILEMNG/FILEMNG.C
@@ -331,8 +331,8 @@ int create_dir(struct directory dir,struct directory folder){//TODOO: check if d
     dmph((char*)base,folder_size+2*sizeof(directory_size),16);
     nl();
     modify_dir(folder,(char*) base,folder_size+2*sizeof(directory_size));
-    while(1){}
     dalloc((uint32_t)base,folder_size+2*sizeof(directory_size));
+    while(1){}
     prints("RETURN OF THE DALLOC",21);
     nl();
     return 0;
